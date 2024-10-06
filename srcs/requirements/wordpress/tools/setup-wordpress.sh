b25c1747a230140699bf731307fe01402c4446cb
Sirui <sirui.qiu01@gmail.com>
2024-10-07 01:01:49 +0200

    refactor: use env vars for wordpress

--- srcs/requirements/wordpress/tools/setup-wordpress.sh
+++ srcs/requirements/wordpress/tools/setup-wordpress.sh
@@ -1,11 +1,52 @@
 #!/bin/bash
 
+
+# Logging functions
+log() {
+    echo "[`date +'%Y-%m-%d %H:%M:%S'`] $1" >> /var/log/wordpress/wordpress.log
+}
+
+error() {
+    echo "[`date +'%Y-%m-%d %H:%M:%S'`] ERROR: $1" >&2 >> /var/log/wordpress/wordpress.error.log
+    exit 1
+}
+
+mkdir -p /var/log/wordpress/
+chown -R $(whoami) /var/log/wordpress/
+chmod -R 755 /var/log/wordpress/
+
 cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
 
-# Start php-fpm in foreground with -F
-php-fpm8.2 -F
+log "Download the core WordPress files"
+./wp-cli.phar core download --allow-root
+
+log "Create wp-config.php"
+WORDPRESS_DB_USER_PASSWORD="$(cat /run/secrets/db_user_password)"
+if ! ./wp-cli.phar config create \
+		--dbname=$WORDPRESS_DB_NAME \
+		--dbuser=$WORDPRESS_DB_USER \
+		--dbpass="$WORDPRESS_DB_USER_PASSWORD" \
+		--dbhost=$MARIADB_HOST \
+		--allow-root; then
+	log "Failed to create wp-config.php with the wp-cli."
+else
+    log "wp-config.php created successfully."
+fi
+
+log "Install WordPress"
+WORDPRESS_ADMIN_PASSWORD="$(cat /run/secrets/wordpress_admin_password)"
+if ! ./wp-cli.phar core install \
+		--url=$DOMAIN \
+		--title=$TITLE \
+		--admin_user=$WORDPRESS_ADMIN \
+		--admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+		--admin_email="$WORDPRESS_ADMIN_MAIL" \
+		--allow-root; then
+	log "Failed to install wordpress with the wp-cli."
+else
+    log "wordpress installed successfully."
+fi
+
+exec $@
--- tools/generate-pw.sh
+++ tools/generate-pw.sh
@@ -5,3 +5,6 @@ openssl rand -base64 16 > db_root_password
 
 # Generate a random password (16 characters) for the MariaDB regular user
 openssl rand -base64 16 > db_user_password
+
+# Generate a random password (16 characters) for the Wordpress admin
+openssl rand -base64 16 > wordpress_admin_password
