813cd470eb19d7ce6ad5325ab9fa717fd1cd0df9
sirui <sirui.qiu01@gmail.com>
2024-10-14 13:09:52 +0200

    feat: check for setup

--- srcs/requirements/nginx/tools/setup-nginx.sh
+++ srcs/requirements/nginx/tools/setup-nginx.sh
@@ -11,33 +11,49 @@ error() {
 }
 
 # Generate Diffie-Hellman (DH) parameters for SSL/TLS key exchange
-certtool --generate-dh-params --sec-param High --outfile /etc/nginx/dhparam.pem
+if [ ! -f /etc/nginx/dhparam.pem ] || ! openssl dhparam -in /etc/nginx/dhparam.pem -check; then
+    log "Generating Diffie-Hellman (DH) parameters for SSL/TLS key exchange"
+    certtool --generate-dh-params --sec-param High --outfile /etc/nginx/dhparam.pem
+else
+    log "DH parameters already exist"
+fi
+
 
 # Create a Configuration Snippet Pointing to the SSL Key and Certificate
-touch /etc/nginx/snippets/self-signed.conf
-echo "ssl_certificate /run/secrets/site_cert;
-ssl_certificate_key /run/secrets/site_key;" > /etc/nginx/snippets/self-signed.conf
+if [ ! -f /etc/nginx/snippets/self-signed.conf ]; then
+    log "Creating a Configuration Snippet Pointing to the SSL Key and Certificate"
+    touch /etc/nginx/snippets/self-signed.conf
+    echo "ssl_certificate /run/secrets/site_cert;
+    ssl_certificate_key /run/secrets/site_key;" > /etc/nginx/snippets/self-signed.conf
+else
+    log "Configuration Snippet Pointing to the SSL Key and Certificate already exists"
+fi
+
 
 # Create a Configuration Snippet with Strong Encryption Settings
-touch /etc/nginx/snippets/ssl-params.conf
-echo "ssl_protocols TLSv1.3;
-ssl_prefer_server_ciphers on;
-ssl_dhparam /etc/nginx/dhparam.pem; 
-ssl_ciphers EECDH+AESGCM:EDH+AESGCM;
-ssl_ecdh_curve secp384r1;
-ssl_session_timeout  10m;
-ssl_session_cache shared:SSL:10m;
-ssl_session_tickets off;
-ssl_stapling on;
-ssl_stapling_verify on;
-resolver 8.8.8.8 8.8.4.4 valid=300s;
-resolver_timeout 5s;
-add_header Strict-Transport-Security \"max-age=63072000; includeSubDomains; preload\";
-add_header X-Frame-Options DENY;
-add_header X-Content-Type-Options nosniff;
-add_header X-XSS-Protection \"1; mode=block\";" > /etc/nginx/snippets/ssl-params.conf
+if [ ! -f /etc/nginx/snippets/ssl-params.conf ]; then
+    log "Creating a configuration snippet with strong encryption settings"
+    touch /etc/nginx/snippets/ssl-params.conf
+    echo "ssl_protocols TLSv1.3;
+    ssl_prefer_server_ciphers on;
+    ssl_dhparam /etc/nginx/dhparam.pem; 
+    ssl_ciphers EECDH+AESGCM:EDH+AESGCM;
+    ssl_ecdh_curve secp384r1;
+    ssl_session_timeout  10m;
+    ssl_session_cache shared:SSL:10m;
+    ssl_session_tickets off;
+    ssl_stapling on;
+    ssl_stapling_verify on;
+    resolver 8.8.8.8 8.8.4.4 valid=300s;
+    resolver_timeout 5s;
+    add_header Strict-Transport-Security \"max-age=63072000; includeSubDomains; preload\";
+    add_header X-Frame-Options DENY;
+    add_header X-Content-Type-Options nosniff;
+    add_header X-XSS-Protection \"1; mode=block\";" > /etc/nginx/snippets/ssl-params.conf
+else
+    log "Configuration snippet with strong encryption settings already exists"
+fi
 
-log "Starting nginx"
 # Pass as a global directive ensuring it applies to the entire nginx process including spawned
 # worker processes. It is necessary when running Nginx inside a Docker
 # container because Docker expects the main process (in this case, Nginx)
@@ -46,4 +62,5 @@ log "Starting nginx"
 #
 # -g: pass global directives to the nginx process
 # daemon off: run nginx in the foreground
+log "Starting nginx"
 nginx -g 'daemon off;'
