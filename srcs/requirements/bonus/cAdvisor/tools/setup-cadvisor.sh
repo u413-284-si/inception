a81023d2cd2439eff242fa42882ddbfe9b91564f
sirui <sirui.qiu01@gmail.com>
2024-10-17 16:58:37 +0200

    feat: add cadvisor

---
+++ srcs/requirements/bonus/cAdvisor/tools/setup-cadvisor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+# Logging functions
+log() {
+    echo "[`date +'%Y-%m-%d %H:%M:%S'`] $1"
+}
+
+error() {
+    echo "[`date +'%Y-%m-%d %H:%M:%S'`] ERROR: $1" >&2
+    exit 1
+}
+
+log "Starting cAdvisor"
+exec $@
